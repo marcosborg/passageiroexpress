562bf1df303bd8442dd47df5ecaf0003e9fa9faa
marcosborg <m.borges.mail@gmail.com>
2025-03-31 11:38:42 +0100

    Update hero banner styles for improved visibility and readability

--- resources/views/components/website/hero_banner.blade.php
+++ resources/views/components/website/hero_banner.blade.php
@@ -3,11 +3,11 @@
     <div class="container">
         <div class="row align-items-center">
             <div class="spacer-double sm-hide"></div>
-            <div class="col-lg-6">
-                <h4><span class="id-color">{{ $hero_banner->subtitle ?? '' }}</span></h4>
+            <div class="col-lg-6" style="background-size: 100%;background-repeat: no-repeat;color: #ffffff;background-color: #000000a1;padding: 20px;">
+                <h4><span class="id-color" style="color: #ffffff;">{{ $hero_banner->subtitle ?? '' }}</span></h4>
                 <div class="spacer-10"></div>
-                <h1>{{ $hero_banner->title ?? '' }}</h1>
-                <p class="lead">{{ $hero_banner->text ?? '' }}</p>
+                <h1 style="color: #ffffff;">{{ $hero_banner->title ?? '' }}</h1>
+                <p class="lead" style="color: #ffffff">{{ $hero_banner->text ?? '' }}</p>
 
                 <a class="btn-main" href="{{ $hero_banner->link ?? '' }}">{{ $hero_banner->button ?? '' }}</a>
             </div>
